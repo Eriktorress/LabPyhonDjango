25a209be0c7667e57b25c93bae2527784e85a7a7
Joan Toro Ortiz <joantoro@me.com>
2022-05-23 21:50:51 -0400

    Proyecto base + app

---
+++ todo/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class Tarea (models.Model):
+    titulo = models.TextField(verbose_name="Titulo", blank=False, null=False, max_length=150)
